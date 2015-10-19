bca463448a718b7e9f2e9b243298ce36d512754b
YAMAMOTO Takashi <yamamoto@midokura.com>
2015-10-19 15:12:15 +0900

    tempest plugin
    Change-Id: I8f894cd11a789013d5b94c58cdccbf6a6b846112

---
+++ neutron_fwaas/tests/tempest_plugin/plugin.py
@@ -0,0 +1,23 @@
+
+import os
+import sys
+
+from tempest.test_discover import plugin
+
+
+class NeutronFWaaSPlugin(plugin.TempestPlugin):
+    def get_opt_lists(self):
+        return []
+
+    def load_tests(self):
+        this_dir = os.path.dirname(os.path.abspath(__file__))
+        # top_level_dir = $(this_dir)/../../..
+        d = os.path.split(this_dir)[0]
+        d = os.path.split(d)[0]
+        top_level_dir = os.path.split(d)[0]
+        test_dir = os.path.join(top_level_dir,
+            'neutron_fwaas/tests/tempest_plugin/tests/scenario')
+        return (test_dir, top_level_dir)
+
+    def register_opts(self):
+        return
---
+++ neutron_fwaas/tests/tempest_plugin/tests/scenario/test_fwaas.py
@@ -0,0 +1,27 @@
+
+import six
+from tempest_lib.common.utils import data_utils
+from tempest_lib import exceptions as lib_exc
+
+from tempest import config
+from tempest.scenario import manager
+from tempest import test
+
+CONF = config.CONF
+
+
+class TestFWaaS(manager.NetworkScenarioTest):
+
+    @classmethod
+    def resource_setup(cls):
+        super(TestFWaaS, cls).resource_setup()
+        if not test.is_extension_enabled('fwaas', 'network'):
+            msg = "FWaaS Extension not enabled."
+            raise cls.skipException(msg)
+        cls.fw_rule = cls.create_firewall_rule("allow", "tcp")
+        cls.fw_policy = cls.create_firewall_policy()
+
+    @test.idempotent_id('1b84cf01-9c09-4ce7-bc72-b15e39076468')
+    def test_firewall(self):
+        network, subnet, router = self.create_networks()
+        network, subnet, router = self.create_networks()
