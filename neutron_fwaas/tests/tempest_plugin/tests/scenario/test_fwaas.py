265354e80b3393059f674bb7955c3f2f340a46b4
YAMAMOTO Takashi <yamamoto@midokura.com>
2015-10-19 17:44:48 +0900

    wip
    Change-Id: I46fc05b64e52f18a622dc2f5005eedb22890ce66

--- neutron_fwaas/tests/tempest_plugin/tests/scenario/test_fwaas.py
+++ neutron_fwaas/tests/tempest_plugin/tests/scenario/test_fwaas.py
@@ -61,6 +61,7 @@ class TestFWaaS(base.FWaaSScenarioTest):
 
         server1, keys1 = self._create_server(network1)
         server2, keys2 = self._create_server(network2)
+        self.assertEqual({}, server1)
         access_point = self._ssh_to_server(self._server_ip(server1, network1),
                                            keys1['private_key'])
         self.assertEqual([], server2)
